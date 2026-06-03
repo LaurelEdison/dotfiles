71aa9b69a9bb0537c7ca7f3e72f6cc5755985127
Ferdinand Laurel Edison <ferdinand@laureledison.org>
2026-06-03 12:03:17 +0700

    post-install: create post install script for dev tools

---
+++ post-install/dev-tools-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+echo "Updating system..."
+sudo pacman -Syu
+
+PACKAGES=(
+    base-devel
+    git
+    github-cli
+    gitlab
+    vim
+    neovim
+    npm
+    nvm
+    rustup
+    docker
+    docker-compose
+    go
+    golangci-lint
+    jdk17-openjdk
+    npm
+    nvm
+    openssh
+    openssl
+    python
+    rustup
+    zip
+    unzip
+    vegeta
+    valgrind
+)
+
+echo "Installing packages..."
+sudo pacman -S "${PACKAGES[@]}"
+
+echo "Done."
